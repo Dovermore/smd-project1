d81934717195a33eda24df97f72e239b62ccf77a
yangxvlin <1768528843@qq.com>
2019-01-25 17:05:53 +1100

    change code

--- code/hello_world.java
+++ code/hello_world.java
@@ -1,7 +1,8 @@
 //FirstProgram
 
-public class FirstProgram {
+public class hello_world {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        // asdfghjk
     }
 }
