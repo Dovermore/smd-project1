a272eed91592edeb6a2d3fac98c6fad33a821bf0
yangxvlin <1768528843@qq.com>
2019-04-30 20:43:20 +1000

    add comments in automail

--- 2019s1p1/automail/strategies/Automail.java
+++ 2019s1p1/automail/strategies/Automail.java
@@ -12,7 +12,12 @@ public class Automail {
     private ArrayList<IRobot> currentStepIRobotList;
     private ArrayList<IRobot> nextStepIRobotList;
     private IMailPool mailPool;
-    
+
+    /**
+     * create AutoMail with two components (mail pool and robots) in the system
+     * @param delivery: Confirm the delivery and calculate the total score
+     * @param numRobots: number of robots in the building
+     * */
     public Automail(IMailDelivery delivery, int numRobots) {
     	// Swap between simple provided strategies and your strategies here
     	    	
@@ -28,6 +33,9 @@ public class Automail {
         }
     }
 
+    /**
+     * step every component in the system
+     * */
     public void step() throws InvalidDispatchException {
         this.mailPool.step();
 
@@ -39,10 +47,15 @@ public class Automail {
         this.nextStepIRobotList.clear();
     }
 
+    /** return the mail pool in the auto mail */
     public IMailPool getMailPool() {
         return mailPool;
     }
 
+    /**
+     * add new IRobot to be stepped in the auto mail system
+     * @param robot: robot or robot team to be stepped in next time frame
+     * */
     public void addIRobot(IRobot robot) {
         assert robot != null;
     }
