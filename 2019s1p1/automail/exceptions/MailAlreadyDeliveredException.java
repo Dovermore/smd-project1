3d5373af897e0aefbdd3bc48a342957ef4de0bab
yangxvlin <1768528843@qq.com>
2019-05-03 15:07:47 +1000

    exceptions added comments

--- 2019s1p1/automail/exceptions/MailAlreadyDeliveredException.java
+++ 2019s1p1/automail/exceptions/MailAlreadyDeliveredException.java
@@ -1,8 +1,14 @@
 package exceptions;
 
 /**
- * An exception thrown when a mail that is already delivered attempts to be delivered again.
+ * Team Number: WS12-3
+ * Group member: XuLin Yang(904904), Zhuoqun Huang(908525), Renjie Meng(877396)
+ *
+ * @create 2019-5-3 15:04:25
+ * description: An exception thrown when a mail that is already delivered
+ *              attempts to be delivered again.
  */
+
 public class MailAlreadyDeliveredException extends Throwable    {
     public MailAlreadyDeliveredException(){
         super("This mail has already been delivered!");
